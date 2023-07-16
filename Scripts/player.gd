a86958138c3705788a2c576d4bd209e736b1c2c0
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-17 01:52:18 +0200

    attack now works

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed : float = 10
+@export var attack : Area2D
 @export var ability : TextureProgressBar
 var tween : Tween
 var timer = 100
@@ -19,7 +20,23 @@ func _physics_process(delta):
 		
 	ability.value = timer
 	if Input.is_action_just_pressed("player_action") and timer == 100:
+		#Attack 
+		attack.visible = true
+		attack.monitoring = true
+		var _timer = Timer.new()
+		add_child(_timer)
+		_timer.wait_time = 0.1
+		_timer.one_shot = true
+		_timer.start()
+		_timer.timeout.connect(attack_end)
+		
+		
 		tween = get_tree().create_tween();
 		timer = 0
 		tween.parallel().tween_property(self, "timer", 100, 1.2)
+		
+		
+func attack_end():
+	attack.visible = false
+	attack.monitoring = false
 		
