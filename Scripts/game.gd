6b96a9b418379918bbe685e4d580c02705e4c16f
Booklordofthedings <jannis.v.hagen@gmail.com>
2023-07-17 21:44:10 +0200

    barrel spawning

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,25 +1,23 @@
 extends Node
-
+@export var curve : Curve
 @export var toSpawn : PackedScene
 @export var lbl : Label
 var timer = 1;
-var spawn_delay = 5
+var spawn_timer = 0
 
 func _ready():
 	get_node("/root/Scorekeeper").start_scoring()
 	randomize()
-	stunned_tween = get_tree().create_tween()
-	stunned_tween.set_trans(Tween.TRANS_QUAD)
-	stunned_tween.tween_property(self, "spawn_delay", 0.1, 40)
 
 
 var stunned_tween : Tween
 
 func _process(delta):
+	spawn_timer += delta
 	lbl.text = str(round(get_node("/root/Scorekeeper").score))
 	
-	if timer < 0:
-		timer = spawn_delay
+	if timer <= 0:
+		timer = curve.sample(min(spawn_timer/140,1))
 		var spwn = toSpawn.instantiate()
 		add_child(spwn)
 	timer = timer - delta
